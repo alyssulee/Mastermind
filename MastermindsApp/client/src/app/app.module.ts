06fceb10c401eee0d0f361a6c381fa655818a20f
Julia Grab <julia.grab@ucalgary.ca>
2022-04-01 12:03:53 -0600

    Completed the rules popup

--- MastermindsApp/client/src/app/app.module.ts
+++ MastermindsApp/client/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DialogModule } from "primeng/dialog";
+import { ButtonModule } from "primeng/button";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateGameComponent } from './create-game/create-game.component';
 import { JoinGameComponent } from './join-game/join-game.component';
 import { RulesButtonComponent } from './rules-button/rules-button.component';
-
 
 @NgModule({
   declarations: [
@@ -18,6 +20,9 @@ import { RulesButtonComponent } from './rules-button/rules-button.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule, 
+    DialogModule, 
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- MastermindsApp/client/src/app/rules-button/rules-button.component.ts
+++ MastermindsApp/client/src/app/rules-button/rules-button.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PrimeNGConfig } from "primeng/api";
 
 @Component({
   selector: 'app-rules-button',
   templateUrl: './rules-button.component.html',
   styleUrls: ['./rules-button.component.scss']
 })
-export class RulesButtonComponent implements OnInit {
 
-  constructor() { }
+export class RulesButtonComponent {
 
-  ngOnInit(): void {
+  constructor(private primengConfig: PrimeNGConfig) {
+    this.displayRules = false;
   }
 
-  openRulesPopup() {
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
 
+  displayRules: boolean;
+
+  openRulesPopup(): void {
+    this.displayRules = true;
   }
 }
