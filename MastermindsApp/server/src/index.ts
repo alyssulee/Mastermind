4c1137103129f7f2ae1dbd7214c70000c33da687
Alyssa Lee <alyssalee669@gmail.com>
2022-03-31 20:54:15 -0600

    changed js to ts file

--- MastermindsApp/server/src/index.ts
+++ MastermindsApp/server/src/index.ts
@@ -3,19 +3,10 @@ const PORT = 8080;
 var express = require('express'),
     app = express(), 
     server = require('http').createServer(app),
-    io =require('socket.io')(server, {
-        cors: {
-          origin: '*',
-        }
-      }),
+    io =require('socket.io')(server, { cors: { origin: '*', } }),
     path = require('path');
 server.listen(PORT);
 console.log("Server Running on Port ", PORT);
-
-
-app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/public/index.html');
-});
 
 io.on('connection', (socket) => 
 {
