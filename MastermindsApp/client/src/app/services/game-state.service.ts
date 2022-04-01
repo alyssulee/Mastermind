2b4a056fc9f6a3a480443612cb544eb8241792d7
Alyssa Lee <alyssalee669@gmail.com>
2022-04-01 17:09:02 -0600

    Updated gameService to be a singleton service

--- MastermindsApp/client/src/app/services/game-state.service.ts
+++ MastermindsApp/client/src/app/services/game-state.service.ts
@@ -2,17 +2,18 @@ import { Injectable } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
 import { Observable } from 'rxjs';
 import { GameWord } from '../interfaces/GameWord';
+import { GameService } from './game-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class GameStateService
 {
   private socket: Socket;
 
-  constructor() { 
-    // TODO: access GameService socket instead of creating a new one...
-    this.socket = io('http://localhost:8080');  
+  constructor(private gameService : GameService) { 
+    this.socket = this.gameService.socket; 
     this.sendGenerateWordEvent();
   }
 
