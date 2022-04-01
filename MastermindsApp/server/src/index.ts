a0ad694721d9ba9dd28918cf00057709f548f4ae
Alyssa Lee <alyssalee669@gmail.com>
2022-03-31 21:27:48 -0600

    Fixed 'require' error

--- MastermindsApp/server/src/index.ts
+++ MastermindsApp/server/src/index.ts
@@ -1,9 +1,10 @@
+declare var require: any
 const PORT = 8080;
 
 var express = require('express'),
     app = express(), 
     server = require('http').createServer(app),
-    io =require('socket.io')(server, { cors: { origin: '*', } }),
+    io = require('socket.io')(server, { cors: { origin: '*', } }),
     path = require('path');
 server.listen(PORT);
 console.log("Server Running on Port ", PORT);
