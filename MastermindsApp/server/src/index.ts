87b956eee6362a6bf76efcc6e9fa0ea33f2a6d66
Alyssa Lee <alyssalee669@gmail.com>
2022-03-31 22:38:06 -0600

    Added WordService and tsconfig file

--- MastermindsApp/server/src/index.ts
+++ MastermindsApp/server/src/index.ts
@@ -1,9 +1,12 @@
+import { WordService } from "./services/WordService";
+
 const PORT = 8080;
+var wordService = new WordService();
 
 var express = require('express'),
     app = express(), 
     server = require('http').createServer(app),
-    io =require('socket.io')(server, { cors: { origin: '*', } }),
+    io = require('socket.io')(server, { cors: { origin: '*', } }),
     path = require('path');
 server.listen(PORT);
 console.log("Server Running on Port ", PORT);
@@ -12,3 +15,4 @@ io.on('connection', (socket) =>
 {
     console.log(socket.id, ': user connected');
 });
+
