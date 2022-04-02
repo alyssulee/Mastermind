fe332e71f876a0c36d23c4203fda116ac0801a19
Alyssa Lee <alyssalee669@gmail.com>
2022-04-01 18:54:41 -0600

    Fixed socket subscriptions

--- MastermindsApp/server/src/services/EventHandlers/WordHandler.ts
+++ MastermindsApp/server/src/services/EventHandlers/WordHandler.ts
@@ -3,8 +3,11 @@ import { WordService } from "../WordService";
 var wordService = new WordService();
 
 module.exports = (io, socket) => {
-    const generateWordSet = (roomCode : string) => {
+    const generateWordSet = () => {
+        let rooms = socket.rooms; 
+        let roomCode = [...rooms][1];
         let wordSet = wordService.GenerateWordSet();
+
         io.to(roomCode).emit("words:generated-set", wordSet);
     }
   
