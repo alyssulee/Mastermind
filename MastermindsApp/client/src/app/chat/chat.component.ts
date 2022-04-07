01585989f89a8fb2661294dc43c1caa3417c8d0c
HumbleChaudhry <humblechaudhry@live.ca>
2022-04-07 01:47:45 -0600

    Added a chat box onto the page

---
+++ MastermindsApp/client/src/app/chat/chat.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Clue, Role, Team } from '../interfaces/GameLogicInterfaces';
+import { GameStateService } from '../services/game-state.service';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss'],
+})
+export class ChatComponent implements OnInit {
+  isMastermind: boolean;
+  constructor(private gameState: GameStateService) {
+    this.isMastermind = gameState.user.role == Role.Mastermind;
+  }
+
+  ngOnInit(): void {}
+}
