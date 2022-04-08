31e6ca5547a9baa0efb528498234020a2e05a558
HumbleChaudhry <humblechaudhry@live.ca>
2022-04-08 17:44:44 -0600

    Fixed Create Room Username

--- MastermindsApp/client/src/app/services/room.service.ts
+++ MastermindsApp/client/src/app/services/room.service.ts
@@ -5,75 +5,82 @@ import { GameService } from './game-service.service';
 import { joinRequest } from '../interfaces/JoinRequest';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-export class RoomService
-{
+export class RoomService {
   private socket: Socket;
   roomcode: string;
 
-  constructor(private gameService : GameService) { 
+  constructor(private gameService: GameService) {
     this.socket = this.gameService.socket;
-    this.roomcode = "";
+    this.roomcode = '';
   }
 
-  onNewRoomRequested (nickname: string) {
+  onNewRoomRequested(nickname: string) {
     this.socket.emit('room:request-room-creation', nickname);
   }
 
-  onRequestToJoinRoom (nickname: string, roomCode: string) {
-    let joinRequest:joinRequest = {
+  onRequestToJoinRoom(nickname: string, roomCode: string) {
+    let joinRequest: joinRequest = {
       nickname: nickname,
-      roomCode: roomCode
-    }
+      roomCode: roomCode,
+    };
 
     this.socket.emit('room:request-to-join', JSON.stringify(joinRequest));
   }
-  
-  onJoinedRoom () {
-    return new Observable<string>(observer => {
-      this.socket.on('room:joined-room', roomCode => {
+
+  onJoinedRoom() {
+    return new Observable<string>((observer) => {
+      this.socket.on('room:joined-room', (roomCode) => {
         this.roomcode = roomCode;
         observer.next(roomCode);
       });
     });
   }
 
-  onNicknameEmptyCreate(){
-    return new Observable<string>(observer => {
+  onJoinedCreatedRoom() {
+    return new Observable<string>((observer) => {
+      this.socket.on('room:joined-created-room', (roomCode) => {
+        this.roomcode = roomCode;
+        observer.next(roomCode);
+      });
+    });
+  }
+
+  onNicknameEmptyCreate() {
+    return new Observable<string>((observer) => {
       this.socket.on('room:nickname-empty-create', () => {
         observer.next();
       });
     });
   }
 
-  onNicknameEmptyJoin(){
-    return new Observable<string>(observer => {
+  onNicknameEmptyJoin() {
+    return new Observable<string>((observer) => {
       this.socket.on('room:nickname-empty-join', () => {
         observer.next();
       });
     });
   }
 
-  onNicknameUsed(){
-    return new Observable<string>(observer => {
+  onNicknameUsed() {
+    return new Observable<string>((observer) => {
       this.socket.on('room:user-already-exists', () => {
         observer.next();
       });
     });
   }
 
-  onRoomDoesNotExist(){
-    return new Observable<string>(observer => {
+  onRoomDoesNotExist() {
+    return new Observable<string>((observer) => {
       this.socket.on('room:room-does-not-exist', () => {
         observer.next();
       });
     });
   }
 
-  onMaxCapacityReached(){
-    return new Observable<string>(observer => {
+  onMaxCapacityReached() {
+    return new Observable<string>((observer) => {
       this.socket.on('room:max-capacity', () => {
         observer.next();
       });
